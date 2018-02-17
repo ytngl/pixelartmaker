dfc2fa9bafc73aea8582b6fcefa2ba571f68761d
Yohana Tania Yap <taniayap@gmail.com>
2018-02-17 13:48:47 -0800

    first commit

---
+++ design.js
@@ -0,0 +1,47 @@
+console.log("js loaded")
+
+function makeGrid() {
+    console.log("makeGrid is running!")
+
+    // Select size input
+
+    var canvas, cell, gridHeight, gridWidth, rows;
+
+    canvas = $('#pixelCanvas');
+    gridHeight = $('#inputHeight').val();
+    gridWidth = $('#inputWidth').val();
+
+    canvas.children().remove()
+
+    for (x = 0; x < gridHeight; x++) {
+        canvas.append('<tr></tr>');
+    }
+
+    rows = $('tr');
+
+    for (y = 0; y < gridWidth; y++) {
+        rows.append('<td></td>');
+    } 
+
+    cell = canvas.find('td');
+
+    // When td is clicked by the user, change color of td
+    cell.click(function() {
+        // Select color input
+        console.log("changeColor is running!");
+        var color;
+        color = $("#colorPicker").val();
+        $(this).attr('bgcolor', color);
+    });
+
+}
+
+// When size is submitted by the user, call makeGrid()
+var submitQuery;
+
+submitQuery = $('input[type="submit"]');
+
+submitQuery.click(function(event) {
+    event.preventDefault();
+    makeGrid();
+});
